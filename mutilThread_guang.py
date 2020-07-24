f65d374be042dc6f0d2db152d920ebf6df7c56df
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-24 08:39:29 +0300

    Get away from master and slave lingo

--- mutilThread_guang.py
+++ mutilThread_guang.py
@@ -1287,7 +1287,7 @@ CREATE TABLE `user` (
   `Create_tmp_table_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
   `Lock_tables_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
   `Execute_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Repl_slave_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Repl_subordinate_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
   `Repl_client_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
   `Create_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
   `Show_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
